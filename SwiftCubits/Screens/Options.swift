76d030e3ca3f59e47e780b56d3bad8ff375330a3
Ethan Chen <ethan.wh.chen@gmail.com>
2024-01-03 13:37:05 -0800

    Added nav bar

---
+++ SwiftCubits/Screens/Options.swift
@@ -0,0 +1,18 @@
+//
+//  Options.swift
+//  SwiftCubits
+//
+//  Created by Wei Chen on 1/3/24.
+//
+
+import SwiftUI
+
+struct Options: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Options()
+}
---
+++ SwiftCubits/Screens/Profile.swift
@@ -0,0 +1,18 @@
+//
+//  Profile.swift
+//  SwiftCubits
+//
+//  Created by Wei Chen on 1/3/24.
+//
+
+import SwiftUI
+
+struct Profile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Profile()
+}
