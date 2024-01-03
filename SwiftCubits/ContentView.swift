cfd4e87849ed7f49dc7e5a5ac28dc8168c95efb6
Ethan Chen <ethan.wh.chen@gmail.com>
2024-01-03 13:04:46 -0800

    Initial Commit

---
+++ SwiftCubits/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftCubits
+//
+//  Created by Wei Chen on 1/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("This is a test")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftCubits/SwiftCubitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCubitsApp.swift
+//  SwiftCubits
+//
+//  Created by Wei Chen on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCubitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
