71f910968b80ac01960a507b93f69b31895b7b16
Ethan Chen <ethan.wh.chen@gmail.com>
2024-01-05 14:28:17 -0800

    updated nav bar

--- SwiftCubits/Screens/Build.swift
+++ SwiftCubits/Screens/Build.swift
@@ -1,18 +1,18 @@
 //
-//  Options.swift
+//  Build.swift
 //  SwiftCubits
 //
-//  Created by Wei Chen on 1/3/24.
+//  Created by Wei Chen on 1/5/24.
 //
 
 import SwiftUI
 
-struct Options: View {
+struct Build: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    Options()
+    Build()
 }
