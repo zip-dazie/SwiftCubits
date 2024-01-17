199d3f934ca2f5e1cb48dbcaac5fece8faeb0d71
Ethan Chen <ethan.wh.chen@gmail.com>
2024-01-17 01:07:56 -0800

    Restructured navigation, with persisting navbar

--- SwiftCubits/ContentView.swift
+++ SwiftCubits/ContentView.swift
@@ -8,40 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tabSelected: Tab = .house
-    
-    init() {
-        UITabBar.appearance().isHidden = true
-    }
-    
+    @State private var selectedTab: Views = .home
     var body: some View {
-        ZStack {
-            VStack {
-                TabView(selection: $tabSelected) {
-                    ForEach(Tab.allCases, id: \.rawValue) { tab in
-                        HStack {
-                            Image(systemName: tab.rawValue)
-                            Text("\(tab.rawValue.capitalized)")
-                                .bold()
-                                
-                                .animation(nil, value: tabSelected)
-                        }
-                        .tag(tab)
-                        
-                    }
+        switch selectedTab {
+        case .home:
+            NavigationStack{
+                VStack{
+                    Home()
+                    Spacer()
                 }
+               
             }
-            VStack {
-                Spacer()
-                CustomTabBar(selectedTab: $tabSelected)
+        case .profile:
+            NavigationStack{
+                VStack{
+                    Profile()
+                    Spacer()
+                }
+                
+            }
+        case .build:
+            NavigationStack{
+                VStack{
+                    Build()
+                    Spacer()
+                }
+               
             }
         }
+        CustomBar(selectedTab: $selectedTab)
+            .frame(width: .infinity)
+        
+        
     }
+    
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-            .previewInterfaceOrientation(.portrait)
-    }
+#Preview {
+    ContentView()
 }
