156c8341930ecc3afe419830b61a192e79a32b44
Ethan Chen <ethan.wh.chen@gmail.com>
2024-01-24 12:09:22 -0800

    tinkering with 3d modeling; import code

---
+++ SwiftCubits/Model.swift
@@ -0,0 +1,18 @@
+//
+//  3DLoad.swift
+//  SwiftCubits
+//
+//  Created by Wei Chen on 1/19/24.
+//
+
+import SwiftUI
+
+struct _DLoad: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _DLoad()
+}
