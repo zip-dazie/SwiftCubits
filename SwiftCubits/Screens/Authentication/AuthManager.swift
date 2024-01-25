8ef843717eb2f359f656bbf520a54eda1c6ad746
artchann <artchann@gmail.com>
2024-01-25 06:51:46 -0800

    Added base Auth files to communicate with backend; TODO: resign in to app

---
+++ SwiftCubits/Screens/Authentication/AuthManager.swift
@@ -0,0 +1,39 @@
+//
+//  AuthManager.swift
+//  SwiftCubits
+//
+//  Created by Arthur Chan on 1/25/24.
+//
+
+import Foundation
+import FirebaseAuth
+
+struct AuthDataResultModel{ // local copy of authentication
+    let uid: String
+    let email: String? //optional string
+    let photoURL: String? //optional string
+    
+    init(user: User){ //user sourced from FB sdk
+        self.uid = user.uid
+        self.email = user.email
+        self.photoURL = user.photoURL?.absoluteString
+    }
+}
+
+final class AuthManager {
+    
+    static let shared = AuthManager() // FIX THIS SINGLETON -- DEPENDENCY INJECTION
+    private init() { }
+
+    func getAuthUser() throws -> AuthDataResultModel{ //save model locally in SDK, skip database wait
+        guard let user = Auth.auth().currentUser else{
+            throw URLError(.badServerResponse)
+        }
+        return AuthDataResultModel(user: user)
+    }
+    
+    func createUser(email: String, password: String) async throws -> AuthDataResultModel{
+        let authDataResult = try await Auth.auth().createUser(withEmail: email, password: password)
+        return AuthDataResultModel(user: authDataResult.user)
+    }
+}
---
+++ SwiftCubits/Screens/Authentication/AuthView.swift
@@ -0,0 +1,74 @@
+//
+//  AuthView.swift
+//  SwiftCubits
+//
+//  Created by Arthur Chan on 1/25/24.
+//
+
+import Foundation
+import SwiftUI
+
+@MainActor // primary thread 
+final class SignInEmailViewModel: ObservableObject{
+    //published allows dynamic rendering of ui view relative to observable object -- published vars change with view
+    @Published var email = ""
+    @Published var password = ""
+    
+    func signIn(){
+        guard !email.isEmpty, !password.isEmpty else{
+            print ("Error invalid field") //FIX THIS DISPLAY AN ERROR
+            return
+        }
+        
+        Task{
+            do {
+                let returnedUserData = try await AuthManager.shared.createUser(email: email, password: password)
+                print("Success")
+                print(returnedUserData)
+            } catch{
+                print ("Error: \(error)")
+            }
+        }
+    }
+}
+
+struct AuthView: View {
+    @StateObject private var viewModel = SignInEmailViewModel()
+    
+    var body: some View {
+        NavigationStack{
+            VStack{
+                TextField("Email: ", text: $viewModel.email)
+                    .padding()
+                    .background(Color.gray.opacity(0.4))
+                    .cornerRadius(10)
+                
+                SecureField("Password: ", text: $viewModel.password)
+                    .padding()
+                    .background(Color.gray.opacity(0.4))
+                    .cornerRadius(10)
+                
+                Button{
+                    viewModel.signIn()
+                } label: {
+                    Text("Sign in")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(.blue)
+                        .cornerRadius(10)
+                }
+            }
+            .padding()
+            .navigationTitle("Sign in with Email")
+            
+        }
+            
+    }
+}
+
+
+#Preview{
+    AuthView();
+}
