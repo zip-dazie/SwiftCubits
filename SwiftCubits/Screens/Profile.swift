ac0d4dd994074db7546f9843e8aba976733b4a0d
Ethan Chen <ethan.wh.chen@gmail.com>
2024-01-26 02:49:15 -0800

    Adjusted Profile Screen

--- SwiftCubits/Screens/Profile.swift
+++ SwiftCubits/Screens/Profile.swift
@@ -10,9 +10,7 @@ import SwiftUI
 struct Profile: View {
     var body: some View {
         ScrollView{
-            
-            LazyVStack{
-                
+            LazyVStack {
                 Image("catFiller")
                     .resizable()
                     .frame(width: 72, height: 72)
@@ -25,114 +23,107 @@ struct Profile: View {
                 
                 HStack {
                     Text("My Favorites")
-                        .bold()
-                        .font(.system(size: 20))
-                        .padding()
-                    
+                        .fontWeight(.heavy)              .font(.system(size: 23))
+                        .padding([.leading], 20)
                     Spacer()
                 }
                 
-                ScrollView(.horizontal){
-                    LazyHStack {
-                        Image("catFiller")
-                            .resizable()
-                            .frame(width: 198, height: 260)
-                            .cornerRadius(10)
-                            .padding()
-                        
-                        Image("catFiller")
-                            .resizable()
-                            .frame(width: 198, height: 260)
-                            .cornerRadius(10)
-                            .padding()
-                        
-                        Image("catFiller")
-                            .resizable()
-                            .frame(width: 198, height: 260)
-                            .cornerRadius(10)
-                            .padding()
-                        
-                        Image("catFiller")
-                            .resizable()
-                            .frame(width: 198, height: 260)
-                            .cornerRadius(10)
-                            .padding()
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHStack(spacing: 0) {
+                        ForEach(0..<6, id: \.self) { index in
+                            GeometryReader { geometry in
+                                Image("catFiller")
+                                    .resizable()
+                                    .frame(width: 198, height: 270)
+                                    .cornerRadius(10)
+                                    .scaleEffect(scaleValue(geometry: geometry), anchor: .center)
+                            }
+                            .frame(width: 198, height: 290)
+                            
+                        }
                     }
                 }
+            }
+            
+            HStack {
+                Text("Personalization")
+                    .fontWeight(.heavy)              .font(.system(size: 23))
+                    .padding([.leading], 20)
+                
+                Spacer()
+            }
+            
+            Button(action: {
+                print("clicked")
+            }) {
                 
                 HStack {
-                    Text("Personalization")
-                        .bold()
-                        .font(.system(size: 20))
-                        .padding()
+                    
+                    Image("settingsicon")
+                        .foregroundColor(.purple)
+                    
+                    Text("Settings")
+                        .font(.system(size: 16))
+                        .padding([.leading], 5)
+                        .foregroundColor(.black)
                     
                     Spacer()
-                }
-                
-                Button(action: {
-                    print("clicked")
-                }) {
                     
-                    HStack {
-                        
-                        Image("settingsicon")
-                        
-                        Text("Settings")
-                            .font(.system(size: 16))
-                            .padding([.leading], 5)
-                            .foregroundColor(.black)
-                        
-                        Spacer()
-                        
-                        Image("arrowrighticon")
-                    }
-                    .padding()
-                    .frame(width: 342, height: 65)
-                    .background(
-                        RoundedRectangle(
-                            cornerRadius: 10,
-                            style:.continuous
-                        )
-                        .fill(Color("CustomGray"))
+                    Image("arrowrighticon")
+                }
+                .padding()
+                .frame(width: 342, height: 65)
+                .background(
+                    RoundedRectangle(
+                        cornerRadius: 10,
+                        style:.continuous
                     )
-                    
-                }
-                .padding([.top,.bottom], 5)
-                
-                Button(action: {
-                    print("clicked")
-                }){
-                    HStack {
-                        
-                        Image("notificationsicon")
-                        
-                        Text("Notifications")
-                            .font(.system(size: 16))
-                            .padding([.leading], 5)
-                            .foregroundColor(.black)
-                        
-                        Spacer()
-                        
-                        Image("arrowrighticon")
-                    }
-                    .padding()
-                    .frame(width: 342, height: 65)
-                    .background(
-                        RoundedRectangle(
-                            cornerRadius: 10,
-                            style:.continuous
-                        )
-                        .fill(Color("CustomGray"))
-                    )
-                    
-                }
-                .padding([.top,.bottom], 5)
+                    .fill(Color("CustomGray"))
+                )
                 
             }
-        }
+            .padding([.top,.bottom], 5)
+            
+            Button(action: {
+                print("clicked")
+            }){
+                HStack {
+                    
+                    Image("notificationsicon")
+                    
+                    Text("Notifications")
+                        .font(.system(size: 16))
+                        .padding([.leading], 5)
+                        .foregroundColor(.black)
+                    
+                    Spacer()
+                    
+                    Image("arrowrighticon")
+                }
+                .padding()
+                .frame(width: 342, height: 65)
+                .background(
+                    RoundedRectangle(
+                        cornerRadius: 10,
+                        style:.continuous
+                    )
+                    .fill(Color("CustomGray"))
+                )
+                
+            }
+            .padding([.top,.bottom], 5)
+            
+        } .padding(.bottom, 55)
     }
 }
 
+private func scaleValue(geometry: GeometryProxy) -> CGFloat {
+    let midPoint = UIScreen.main.bounds.width / 2
+    let viewFrame = geometry.frame(in: .global)
+    let distance = abs(midPoint - viewFrame.midX)
+    let scale = max(1.0 - distance / 1000, 0.9) // Adjust 500 and 0.9 as needed
+    return scale
+}
 #Preview {
     Profile()
 }
