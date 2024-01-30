d2532e3f711b5a08fac460b84e0f35da50a8823c
joshmoonnn <joshmoonnn@gmail.com>
2024-01-30 13:27:17 -0800

    added search icon and modified Home screen

--- SwiftCubits/Screens/Home.swift
+++ SwiftCubits/Screens/Home.swift
@@ -8,8 +8,58 @@
 import SwiftUI
 
 struct Home: View {
+    
+    @State private var searchPrompt: String = ""
+    
     var body: some View {
-        Text("Home")
+        ScrollView{
+            
+            LazyVStack{
+                HStack{
+                    Text("Welcome back\n")
+                        .font(.system(size:16))
+                    Text("Peter!")
+                        .bold()
+                        .font(.system(size:24))
+                        .multilineTextAlignment(.leading)
+                    Spacer()
+                    Image("catFiller")
+                        .resizable()
+                        .frame(width: 72, height: 72)
+                        .clipShape(Circle())
+                }
+                .padding(20)
+                
+                HStack {
+                    TextField("Search files...", text: $searchPrompt)
+                        .disableAutocorrection(true)
+                    .textFieldStyle(.roundedBorder)
+                    Spacer()
+                    Image("searchicon")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                }
+                .padding([.leading,.trailing], 80)
+                
+                HStack {
+                    Text("Recent Creations")
+                        .bold()
+                        .font(.system(size: 20))
+                        .padding()
+                    
+                    Spacer()
+                }
+                HStack {
+                    Text("Uploads")
+                        .bold()
+                        .font(.system(size: 20))
+                        .padding()
+                    
+                    Spacer()
+                }
+                
+            }
+        }
         }
     }
 
