b5fdcda828a09a3180a4d838aff4592c48a71284
artchann <artchann@gmail.com>
2024-01-29 17:13:28 -0800

    Added database functionality for authmanager

--- SwiftCubits/Manager/AuthManager.swift
+++ SwiftCubits/Manager/AuthManager.swift
@@ -8,18 +8,6 @@
 import Foundation
 import FirebaseAuth
 
-struct AuthDataResultModel{ // local copy of authentication configured to store in firebase SDK
-    let uid: String
-    let email: String? //optional string
-    let photoURL: String? //optional string
-    
-    init(user: User){ //user sourced from FB sdk
-        self.uid = user.uid
-        self.email = user.email
-        self.photoURL = user.photoURL?.absoluteString
-    }
-}
-
 final class AuthManager{
     
     static let shared = AuthManager() // TODO: fix this singleton, low priority
