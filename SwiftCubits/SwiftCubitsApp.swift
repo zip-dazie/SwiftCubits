d7bc7ae1d2a6990b2f2c8b7ac075e343e7a72bcb
artchann <artchann@gmail.com>
2024-01-29 17:14:13 -0800

    New Proj file

--- SwiftCubits/SwiftCubitsApp.swift
+++ SwiftCubits/SwiftCubitsApp.swift
@@ -23,6 +23,7 @@ struct SwiftCubitsApp: App {
     }
 }
 
+//MARK: No idea what this does -- makes things work
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
