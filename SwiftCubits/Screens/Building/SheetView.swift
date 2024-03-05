a1d84fb9375e3e779557e1f4d2ecb241bb12460e
artchann <artchann@gmail.com>
2024-03-04 22:56:45 -0800

    Anchor for redefining calls to sheetview

--- SwiftCubits/Screens/Building/SheetView.swift
+++ SwiftCubits/Screens/Building/SheetView.swift
@@ -7,21 +7,45 @@
 
 import Foundation
 import SwiftUI
+import SceneKit
 
 struct SheetView: View {
-    let imageName: String
-    @State var primarySCNScene = SingleCubitView()
+    //TODO: Programmatically pass in the names of the correct cubit piece here
+    //var cubitName: String;
+//    let imageName: String
+    
+    @State var primarySCNScene:SCNScene?
+//    let sceneURL:URL
+//   primarySCNScene = (url: "Test_0-1.scn")
+    
+    init(){
+        let sceneURL = Bundle.main.url(forResource: "Test_0-1", withExtension: "scn", subdirectory: "Cubits")
+        
+        if (sceneURL == nil){
+            //TODO: Error handling
+            print("Scene file not found")
+        }
+        
+        do{
+            try primarySCNScene = SCNScene(url: sceneURL!)
+        }
+        catch{
+            //TODO: error handling
+            print("Not found")
+        }
+        
+    }
     
     var body: some View {
         VStack{
-            primarySCNScene
+            Text("x")
+            SingleCubitView(scene: $primarySCNScene)
                 .frame(width: 350, height:350)
         }
-        
-        Text("This is the subscreen for \(imageName)")
+        Text("This is the subscreen for test")
     }
 }
 
 #Preview(){
-    SheetView(imageName: ("image1"))
+    SheetView()
 }
--- SwiftCubits/Screens/Building/SingleCubitView.swift
+++ SwiftCubits/Screens/Building/SingleCubitView.swift
@@ -11,33 +11,31 @@ import SceneKit
 
 struct SingleCubitView: UIViewRepresentable {
 //    let scene = SCNScene()
+    @Binding var scene: SCNScene?
     let box: SCNBox = .init()
 
     func makeUIView(context: Context) -> SCNView {
         let sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: 400, height: 400))
-
-        let scene = SCNScene()
         sceneView.allowsCameraControl = true
         sceneView.autoenablesDefaultLighting = true
         sceneView.scene = scene
-        sceneView.backgroundColor = UIColor.gray
-        
-        
-        let box = SCNNode()
-        box.geometry = SCNBox(width: 3, height: 3, length: 3, chamferRadius: 0)
-        box.geometry?.firstMaterial?.diffuse.contents = UIColor.brown
-        //box.geometry?.firstMaterial?.specular.contents = UIColor.gray
-        
-        scene.rootNode.addChildNode(box)
-        
-        SCNTransaction.begin()
-        box.position = SCNVector3(0,0,-10)
-        SCNTransaction.commit()
-        
         sceneView.allowsCameraControl = true
-//        view.autoenablesDefaultLighting = true
+        sceneView.backgroundColor = UIColor.white
         sceneView.antialiasingMode = .multisampling2X
+        let scene = SCNScene()
         
+//        let box = SCNNode()
+//        box.geometry = SCNBox(width: 3, height: 3, length: 3, chamferRadius: 0)
+//        box.geometry?.firstMaterial?.diffuse.contents = UIColor.brown
+        //box.geometry?.firstMaterial?.specular.contents = UIColor.gray
+//        scene.rootNode.addChildNode(box)
+        
+//        SCNTransaction.begin()
+//        box.position = SCNVector3(0,0,-10)
+//        SCNTransaction.commit()
+        
+        
+//        view.autoenablesDefaultLighting = true
         
         return sceneView
     }
@@ -49,5 +47,6 @@ struct SingleCubitView: UIViewRepresentable {
 }
 
 #Preview{
-    SingleCubitView()
+//    let mockScene = SCNScene.init(named:"Test_0-1")
+    SingleCubitView(scene: .constant(SCNScene.init(named:"Test_0-1")))
 }
