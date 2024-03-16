9889d0518fbfb80974af7af7f5b0e9f50dc3b775
artchann <artchann@gmail.com>
2024-03-16 05:31:08 -0700

    Adjusted 'preinstructions' to display thumbnails centered

--- SwiftCubits/Screens/Building/PreInstructions.swift
+++ SwiftCubits/Screens/Building/PreInstructions.swift
@@ -18,72 +18,93 @@ struct PreInstructions: View{
     @State private var sheetView = false
     @State private var next = false
     //MARK: Programmatically pass these into our grid
-    let cubitNames = ["Cubit_2-1", "Cubit_2-2", "Cubit_5-4", "Cubit_6-2", "Cubit_7-1"]
+    let cubitNames = ["Cubit2-1", "Cubit2-2", "Cubit5-4", "Cubit6-2", "Cubit7-1"]
     var counter: Int = 0
     
     var body: some View {
-        ScrollView{
-            VStack{
-                Text("Asteroid")
-                    .fontWeight(.heavy)
-                    .font(.system(size:30))
-                    .padding()
-                Image("demoShape") //TODO: Replace with asteroid
-                    .resizable()
-                    .frame(width:330, height:300)
-                
-            }
-            .padding([.bottom], 20)
-            HStack{
-                Text("Units needed")
-                    .fontWeight(.bold)
-                    .font(.system(size:20))
-                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
-                    .padding([.leading], 20)
-                Spacer()
-            }
-            LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())]) {
-                            ForEach(cubitNames, id: \.self) { imageName in
-                                //MARK: Temporary fix for programmatic passing
-                                if(imageName == "Cubit_2-1"){
-                                    GridItemView(imageName: imageName){
-                                        SheetView(objectName:imageName)
+        NavigationStack{
+            ScrollView{
+                VStack{
+                    Text("Asteroid")
+                        .fontWeight(.heavy)
+                        .font(.system(size:30))
+                        .padding()
+                    Image("AsteroidLayer") //TODO: Replace with asteroid
+                        .offset(x:-15)
+                        .frame(width:330, height:300)
+                    
+                }
+                .padding([.bottom], 20)
+                HStack{
+                    Text("Units needed")
+                        .fontWeight(.bold)
+                        .font(.system(size:20))
+                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                        .padding([.leading], 20)
+                    Spacer()
+                }
+                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())]) {
+                                ForEach(cubitNames, id: \.self) { imageName in
+                                    //MARK: Temporary fix for programmatic passing
+                                    if(imageName == "Cubit2-1"){
+                                        GridItemView<SheetView>(imageName: imageName){
+                                            SheetView(objectName:imageName)
+                                        }
+                                    }
+                                    else if(imageName == "Cubit2-2"){
+                                        GridItemView(imageName: imageName){
+                                            Cubit_2_2_View(objectName:imageName)
+                                        }
+                                    }
+                                    else if(imageName == "Cubit5-4"){
+                                        GridItemView(imageName: imageName){
+                                            Cubit_5_4_View(objectName:imageName)
+                                        }
+                                    }
+                                    else if(imageName == "Cubit6-2"){
+                                        GridItemView(imageName: imageName){
+                                            Cubit_6_2_View(objectName:imageName)
+                                        }
+                                    }
+                                    //MARK: Unknown render error
+                                    else if(imageName == "Cubit7-1"){
+                                        GridItemView(imageName: imageName){
+                                            Cubit_7_1_View(objectName:imageName)
+                                        }
                                     }
                                 }
-                                else if(imageName == "Cubit_2-2"){
-                                    GridItemView(imageName: imageName){
-                                        Cubit_2_2_View(objectName:imageName)
-                                    }
-                                }
-                                else if(imageName == "Cubit_5-4"){
-                                    GridItemView(imageName: imageName){
-                                        Cubit_5_4_View(objectName:imageName)
-                                    }
-                                }
-                                else if(imageName == "Cubit_6-2"){
-                                    GridItemView(imageName: imageName){
-                                        Cubit_6_2_View(objectName:imageName)
-                                    }
-                                }
-                                //MARK: Unknown render error
-                                else if(imageName == "Cubit_7-1"){
-                                    GridItemView(imageName: imageName){
-                                        Cubit_7_1_View(objectName:imageName)
-                                    }
-                                }
-                            }
-            }
+                }
+        }
+            .padding()
+            NavButton(destination:Layer_1(), icon: "rightArrow", linkText: "Next")
+                .padding(.leading,20)
+        
+        
             
            
         }
     }
 }
 
+struct imageContainer{
+    let imageName: String
+    let offsetX: Int
+    let offsetY: Int
+}
+
 //TODO: PASS INTO THIS OBJECT THE RELATIVE MODEL
 struct GridItemView<Content>: View where Content: View{
     @State private var isPresented = false
+
     let imageName: String
     let content: () -> Content
+    
+   
+//    init(imageName: String, offsetX: Int, offsetY: Int){
+//        self.imageName = imageName
+//        self.offsetX = offsetX
+//        self.offsetY = offsetY
+//    }
     
     var body: some View {
         Button(action: {
@@ -93,9 +114,11 @@ struct GridItemView<Content>: View where Content: View{
                 .frame(width:150, height: 150)
                 .overlay(
                     Image(imageName)
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+//                        .resizable()
+                        .offset(x:-5)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height:100)
+                        
                     
                 )
                 .clipShape(RoundedRectangle(cornerRadius: 20))
