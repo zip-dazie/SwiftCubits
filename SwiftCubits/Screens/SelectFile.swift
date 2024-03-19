3e46736bf4ffc5909ecc7b928eff070af14faffe
Ethan Chen <ethan.wh.chen@gmail.com>
2024-03-18 17:38:16 -0700

    Navigation to Select File

---
+++ SwiftCubits/Screens/SelectFile.swift
@@ -0,0 +1,18 @@
+//
+//  SelectFile.swift
+//  SwiftCubits
+//
+//  Created by Wei (Ethan) Chen on 3/18/24.
+//
+
+import SwiftUI
+
+struct SelectFile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectFile()
+}
